58968ce4d82f9955a33df327c1af8d787ab237bd
Cedric Baus <cedric.baus@me.com>
2020-12-25 21:37:06 +0100

    Added Bot and Connected/LoggedIn Event

--- 4kGamingBot/4kGamingBot/Program.cs
+++ 4kGamingBot/4kGamingBot/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +11,33 @@ namespace _4kGamingBot
 {
     class Program
     {
-        static void Main(string[] args)
+        static DiscordSocketClient _client;
+        private static async Task Main(string[] args)
         {
+            _client = new DiscordSocketClient();
+
+            _client.LoggedIn += Client_LoggedIn;
+            _client.Connected += Client_Connected;
+
+            var token = File.ReadAllText("C:\\Token.txt");
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private static Task Client_Connected()
+        {
+            _client.SetStatusAsync(UserStatus.Online);
+            _client.SetGameAsync("Administrating");
+            return Task.CompletedTask;
+        }
+
+        private static Task Client_LoggedIn()
+        {
+            Console.WriteLine("Logged In");
+            return Task.CompletedTask;
         }
     }
 }
